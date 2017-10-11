de044129eac104308a838b4ef9ef216b3a65a190
petarkrivic <petarkrivic@petars-imac.tel.fer.hr>
2017-10-11 11:46:43 +0200

    -Updated RPC timeout to 30 seconds

--- src/main/java/eu/h2020/symbiote/smeur/dsi/messaging/RabbitManager.java
+++ src/main/java/eu/h2020/symbiote/smeur/dsi/messaging/RabbitManager.java
@@ -130,7 +130,7 @@ public class RabbitManager {
         log.info("Sending RPC message");
 
         String correlationId = UUID.randomUUID().toString();
-        rabbitTemplate.setReplyTimeout(10000);
+        rabbitTemplate.setReplyTimeout(30000);
         Object receivedObj = rabbitTemplate.convertSendAndReceive(exchange, routingKey, obj, new CorrelationData(correlationId));
         if(receivedObj == null) {
             log.info("Timeout in RPC receiving response");
